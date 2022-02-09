920e4f8cc186024c89aa00579749e1fca8deedcf
baolh <baolh@newit.co.jp>
2022-02-10 00:24:36 +0700

    validate

--- pages/decks/_id/index.vue
+++ pages/decks/_id/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Your decks {{$route.params.id}} is active</h1>
+  <h1>Your decks {{ $route.params.id }} is active</h1>
 </template>
 
-<script >
- export  default {
-   name: "IndexID"
- }
+<script>
+export default {
+  name: "IndexID",
+  validate(ctx) {
+
+    return /^[0-9]{9,12}$/.test(ctx.params.id)
+  }
+}
 </script>
